cf851929f061361fe76ea750b8d4ff43498c9704
Carsten Witt <carsten.witt@gmail.com>
2017-09-28 13:00:09 +0200

    Update version

--- cps.rb
+++ cps.rb
@@ -1,8 +1,8 @@
 class Cps < Formula
   desc "Creates a file copy with given suffix."
   homepage "https://github.com/tomkyle/cps"
-  url "https://github.com/tomkyle/cps/archive/1.0.0.tar.gz"
-  sha256 "b36a8925828711b79a28551ac9a02d89c6489a037a54bb5486b68d30b602bc48"
+  url "https://github.com/tomkyle/cps/archive/1.0.1.tar.gz"
+  sha256 "127b2c3025d3b8ccfa6e6b182053b59c474f99c5de69d94d4e6588661a88cc89"
 
   def install
     bin.install "cps"
